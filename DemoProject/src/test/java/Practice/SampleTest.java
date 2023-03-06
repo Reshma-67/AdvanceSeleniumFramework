cc324ba050c75986d50d415378ad1a576eca7281
Reshma674 <reshmamohanty67@gmail.com>
2023-03-06 23:27:57 +0530

    3rd commit

---
+++ DemoProject/src/test/java/Practice/SampleTest.java
@@ -0,0 +1,17 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+@Test
+public class SampleTest
+{
+    private void sample() 
+    {
+		System.out.println("it is a sample program");
+	}
+@Test
+public void m1()
+{
+	System.out.println("m1 running");
+}
+}
